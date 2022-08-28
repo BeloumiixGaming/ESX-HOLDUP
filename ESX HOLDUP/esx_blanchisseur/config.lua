e1dea5b127653a5eb63af2be1da7c48374c67eb0
BeloumiixGaming <110780238+BeloumiixGaming@users.noreply.github.com>
2022-08-29 00:25:19 +0200

    Add files via upload

---
+++ ESX HOLDUP/esx_blanchisseur/config.lua
@@ -0,0 +1,15 @@
+Config					= {}
+Config.Locale			= 'fr'
+Config.Slice			= 1000
+Config.Percentage		= 90
+Config.MarkerSize   	= {x = 3.0, y = 3.0, z = 3.0}
+Config.openHours		= 0
+Config.closeHours		= 5
+Config.Blip 			= false
+Config.Hours 			= false
+Config.Menu				= true 
+Config.Bonus			= {min = 0, max = 0}
+
+Config.Zones = {
+	{x = 786.8830, y = -543.0357, z = 22.47620,percent=30},
+}
